ea89fd72451495576e1811e7cb6cf0d2504d350f
ToughLama <ToughLama@gmail.com>
2016-05-26 16:00:13 +0800

    python getinfo-ipaddress.py x.x.x.x 方式查询IP地址信息

--- getinfo-ipaddress.py
+++ getinfo-ipaddress.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+#coding:utf-8
 
 import os,sys
 import json
 import urllib2
+import re
 
 class taobao():
 	def __init__(self,ip):
@@ -12,25 +14,35 @@ class taobao():
 		re = urllib2.urlopen('http://ip.taobao.com/service/getIpInfo.php?ip='+ self.ip).read()
 		data_json = json.loads(re)
 		re_json = json.dumps(data_json, ensure_ascii=False)
-		#data = json.loads(re_json, encoding='utf-8')
 		data = json.loads(re_json)
+		city = data['data']['city']
+		country = data['data']['country']
 		isp = data['data']['isp']
-		result = self.ip + ':' + isp
+		result = country + ' ' + city + ' ' + isp
 		return result
-		#print  self.ip,':',data['data']['isp']
-		#data_isp = data['data']
+
 
 
 if __name__ == '__main__':
 	reload(sys)
-	sys.setdefaultencoding('utf-8')
-	f = open('/root/ipdatabase.txt', 'a') #IP地址列表
-	for i in open('/root/python/iplist', 'r').readlines(1000000):
-		ip = i.strip('\n')
+	sys.setdefaultencoding('utf-8') #中文编码
+
+	ip = sys.argv[1]
+	regex_ipaddress = re.compile('^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$')
+	if regex_ipaddress.match(ip):
 		m = taobao(ip)
-		result = m.getInfo()
-		print result
-		f.write(result)
-		f.write('\n')
-		time.sleep(0.1)
-	f.close()
+		print m.getInfo()
+		
+	else:
+		print '非法IPv4的地址，请重新输入'
+	
+#	f = open('/root/ipdatabase.txt', 'a') #IP地址列表
+#	for i in open('/root/python/iplist', 'r').readlines(1000000):
+#		ip = i.strip('\n')
+#		m = taobao(ip)
+#		result = m.getInfo()
+#		print result
+#		f.write(result)
+#		f.write('\n')
+#		time.sleep(0.1)
+#	f.close()
